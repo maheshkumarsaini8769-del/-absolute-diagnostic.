a735532cf5567c4ccb6fb0993c1b1b82f980dbcf
maheshkumarsaini8769-del <maheshkumarsaini8769@gmail.com>
2026-09-11 14:06:16 +0530

    fix(pdf): move DOMMatrix polyfill to top-level module scope before imports

--- src/lib/pdf-extraction.ts
+++ src/lib/pdf-extraction.ts
@@ -1,3 +1,45 @@
+// Polyfill browser globals for pdfjs-dist / pdf-parse in Node.js serverless environment
+if (typeof (globalThis as any).DOMMatrix === 'undefined') {
+  ;(globalThis as any).DOMMatrix = class DOMMatrix {
+    a = 1; b = 0; c = 0; d = 1; e = 0; f = 0;
+    m11 = 1; m12 = 0; m13 = 0; m14 = 0;
+    m21 = 0; m22 = 1; m23 = 0; m24 = 0;
+    m31 = 0; m32 = 0; m33 = 1; m34 = 0;
+    m41 = 0; m42 = 0; m43 = 0; m44 = 1;
+    is2D = true;
+    isIdentity = true;
+    constructor(init?: any) {
+      if (Array.isArray(init)) {
+        this.a = init[0] ?? 1; this.b = init[1] ?? 0;
+        this.c = init[2] ?? 0; this.d = init[3] ?? 1;
+        this.e = init[4] ?? 0; this.f = init[5] ?? 0;
+      }
+    }
+    multiply() { return this }
+    translate() { return this }
+    scale() { return this }
+    rotate() { return this }
+    transformPoint(p: any) { return p }
+    inverse() { return this }
+    toString() { return `matrix(${this.a}, ${this.b}, ${this.c}, ${this.d}, ${this.e}, ${this.f})` }
+  }
+}
+
+if (typeof (globalThis as any).Path2D === 'undefined') {
+  ;(globalThis as any).Path2D = class Path2D {
+    addPath() {}
+    closePath() {}
+    moveTo() {}
+    lineTo() {}
+    bezierCurveTo() {}
+    quadraticCurveTo() {}
+    arc() {}
+    arcTo() {}
+    ellipse() {}
+    rect() {}
+  }
+}
+
 import fs from 'fs'
 import path from 'path'
 import crypto from 'crypto'
@@ -33,48 +75,6 @@ export function extractTextFromPDF(buffer: Buffer): string {
 }
 
 export async function extractPDFText(buffer: Buffer): Promise<string> {
-  // Polyfill DOMMatrix for Node.js / Vercel Serverless environment where browser DOM is absent
-  if (typeof (globalThis as any).DOMMatrix === 'undefined') {
-    ;(globalThis as any).DOMMatrix = class DOMMatrix {
-      a = 1; b = 0; c = 0; d = 1; e = 0; f = 0;
-      m11 = 1; m12 = 0; m13 = 0; m14 = 0;
-      m21 = 0; m22 = 1; m23 = 0; m24 = 0;
-      m31 = 0; m32 = 0; m33 = 1; m34 = 0;
-      m41 = 0; m42 = 0; m43 = 0; m44 = 1;
-      is2D = true;
-      isIdentity = true;
-      constructor(init?: any) {
-        if (Array.isArray(init)) {
-          this.a = init[0] ?? 1; this.b = init[1] ?? 0;
-          this.c = init[2] ?? 0; this.d = init[3] ?? 1;
-          this.e = init[4] ?? 0; this.f = init[5] ?? 0;
-        }
-      }
-      multiply() { return this }
-      translate() { return this }
-      scale() { return this }
-      rotate() { return this }
-      transformPoint(p: any) { return p }
-      inverse() { return this }
-      toString() { return `matrix(${this.a}, ${this.b}, ${this.c}, ${this.d}, ${this.e}, ${this.f})` }
-    }
-  }
-
-  if (typeof (globalThis as any).Path2D === 'undefined') {
-    ;(globalThis as any).Path2D = class Path2D {
-      addPath() {}
-      closePath() {}
-      moveTo() {}
-      lineTo() {}
-      bezierCurveTo() {}
-      quadraticCurveTo() {}
-      arc() {}
-      arcTo() {}
-      ellipse() {}
-      rect() {}
-    }
-  }
-
   try {
     // eslint-disable-next-line @typescript-eslint/no-require-imports
     const pdfParseMod = require('pdf-parse')
@@ -94,8 +94,9 @@ export async function extractPDFText(buffer: Buffer): Promise<string> {
         return data.text
       }
     }
-  } catch (err) {
+  } catch (err: any) {
     console.warn('extractPDFText: primary extraction failed, trying stream fallback:', err)
+    return `[PDF_PARSE_ERR: ${err?.message || String(err)}]`
   }
 
   // Fallback: search for text blocks in raw buffer
